fafc16f339d36120692a5f7bf2434682d311ac98
Mykola Saliuk <107276054+Mykola0502@users.noreply.github.com>
2023-02-16 22:06:09 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import { PropTypes } from 'prop-types';
+
+import { ContactItem } from 'components/ContactItem';
+
+export const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map(({ id, name, number }) => (
+          <li key={id}>
+            <ContactItem
+              id={id}
+              name={name}
+              number={number}
+              deleteContact={onDeleteContact}
+            />
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
